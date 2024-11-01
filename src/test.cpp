b54d5732f17abcf4af15dbbb2e40e26ca99e91db
AlexDurandeHuertaJimenez <a23310378@live.ceti.mx>
2024-11-01 01:25:56 +0000

    Clase 4

--- src/test.cpp
+++ src/test.cpp
@@ -13,15 +13,32 @@ int main(int argc, char const *argv[])
     std::string reset_position;
     int frame = 0;
     auto can = Canvas(300,50);
-    can.DrawBlockCircle(50,20,20);
-    can.DrawPointLine(45,24,45,15);
-    can.DrawPointLine(55,24,55,15);
-    can.DrawPointLine(40,30,60,30);
-    can.DrawPointLine(40,30,40,25);
-    can.DrawPointLine(60,30,60,25);
+    can.DrawPointLine(70, 20, 130, 20);         
+    can.DrawPointLine(50, 30, 150, 30);         
+    can.DrawPointLine(50, 10, 50, 30);           
+    can.DrawPointLine(70, 20, 50, 10);
+    can.DrawPointLine(130, 20, 150, 30);
+
+    can.DrawPointLine(75, 40, 105, 25);
+    can.DrawPointLine(75, 40, 90, 25);
+
+    can.DrawPointLine(40, 20, 30, 20);
+    can.DrawPointLine(40, 23, 30, 23);
+    can.DrawPointLine(40, 26, 30, 26);
 
     while (true)
     {
+        auto can = Canvas(300,50);
+        can.DrawPointLine(frame+70, 20, frame+130, 20);
+        can.DrawPointLine(frame+50, 30, frame+150, 30);
+        can.DrawPointLine(frame+50, 10, frame+50, 30);
+        can.DrawPointLine(frame+70, 20, frame+50, 10);
+        can.DrawPointLine(frame+130, 20, frame+150, 30);
+        can.DrawPointLine(frame+75, 40, frame+105, 25);
+        can.DrawPointLine(frame+75, 40, frame+90, 25); 
+        can.DrawPointLine(frame+40, 20, frame+30, 20);
+        can.DrawPointLine(frame+40, 23, frame+30, 23);
+        can.DrawPointLine(frame+40, 26, frame+30, 26);
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
         Element lienzo = bgcolor(Color::Blue, canvas(&can));
         Render(pantalla, lienzo);
